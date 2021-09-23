33ced93cc3ba346a13bb632aee4c43aef3591e8e
Kalashnikov Artsiom <kalashnikov.artsiom@gmail.com>
2021-09-24 01:54:21 +0300

    111

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
-$(function () {
+const submitButtonElement = document.querySelector('#submit-btn');
+const phoneNumberElement = document.querySelector('.phone-number');
+const textAreaElement = document.querySelector('#textarea');
+const validationMessageElement = document.querySelector('.validation-message');
+const myEmail = 'kalashnikov.artsiom@gmail.com';
 
-    $(".head_inner nav ul li.switch a").on("click", function(){
-       $("body").toggleClass("switchMode");
-       e.preventDefault();
+$(function () {
+    $(".head_inner nav ul li.switch a").on("click", function () {
+        $("body").toggleClass("switchMode");
+        e.preventDefault();
     });
+});
+
+submitButtonElement.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    if (!textAreaElement.value) {
+        textAreaElement.value = myEmail;
+    }
+
+
+    const validationMessageSuccess = 'Номер верный';
+    const validationMessageInvalid = 'Номер неверный';
+    const regex = /\+[3][7][5][0-9]{9}/;
+
+    validationMessageElement.classList.add('validation-message__active');
+
+    if (phoneNumberElement.value.match(regex)) {
+        validationMessageElement.classList.remove('validation-message__error');
+        validationMessageElement.classList.add('validation-message__success');
+        validationMessageElement.innerHTML = validationMessageSuccess;
+    }
+
+    if (!phoneNumberElement.value.match(regex)) {
+        validationMessageElement.classList.remove('validation-message__success');
+        validationMessageElement.classList.add('validation-message__error');
+        validationMessageElement.innerHTML = validationMessageInvalid;
+    }
 
 });
